bc59d529319dffc3a2ec9e8cf40e135635b4f731
gmtisrad <gabe.m.timm@gmail.com>
2019-02-13 02:12:17 -0800

    webcrawler finished

--- crawler.py
+++ crawler.py
@@ -1,67 +1,103 @@
 import urllib
 from bs4 import BeautifulSoup
 import re
+import argparse
+from threading import Thread
+import time
+
+parser = argparse.ArgumentParser(description='Recursively crawl the web.')
+parser.add_argument('url', metavar='U', type=str, nargs=(1), help='The base url for the web crawler to start from (eg. \'https://www.google.com/\')')
+parser.add_argument('depth', metavar='D', type=int, nargs=(1), help='The maximum link depth for the web crawler to crawl.')
+parser.add_argument('assets', metavar='A', type=int, nargs=(1), help='1 if page assets will be parsed, 0 if not.')
+args = parser.parse_args()
 
 class WebCrawler(object):
     def __init__(self, url, maxDepth):
         self.MAX_DEPTH = maxDepth
         self.BASE_DEPTH = 0
-        self.BASE_PAGE = -1
+        self.BASE_PAGE = WebPage(url, self.BASE_DEPTH)
         self.BASE_URL = url
-        self.linkIndex = -1
-        print(self.MAX_DEPTH)
+        self.linkIndex = set()
     
     def crawl(self, url, depth):
         page = WebPage(url, depth+1)
-        linkIndexCopy = self.linkIndex.copy()
-        for nestedLink in page.links:
-            if nestedLink not in linkIndexCopy and nestedLink != url and page.depth < self.MAX_DEPTH:
-                self.linkIndex.add(nestedLink)
-                print('Link: %s Depth: %d'%(nestedLink, page.depth))                    
+        newLinks = page.links - self.linkIndex
+
+        for nestedLink in newLinks:
+            if nestedLink != url and page.depth < self.MAX_DEPTH:
+                self.linkIndex.add(nestedLink)                
                 self.crawl(nestedLink, page.depth)
     
     def startCrawl(self):
-        self.BASE_PAGE = WebPage(self.BASE_URL, self.BASE_DEPTH)
         self.linkIndex = self.BASE_PAGE.links
+        startTime = time.time()
+        linkIndexCopy = self.BASE_PAGE.links.copy()
+
+        threads = [Thread(target = self.crawl, args = (link, self.BASE_DEPTH,)) for link in linkIndexCopy]
+
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
         
-        for link in self.BASE_PAGE.links:
-            self.crawl(link, self.BASE_DEPTH)
+        for link in self.linkIndex:
+            print(link)
+
+        print ('Crawl completed after %ds' %(time.time() - startTime))
+
 
         
         
 class WebPage(object):
     def __init__(self, url, depth):
+        self.ASSET_FLAG = args.assets[0]
         self.depth = depth
         self.links = set()
         self.getPageLinks(url)
+        self.size = None
 
     def getPageLinks(self, url):
+        pattern = '(?:(?:https?|ftp):\/\/|\b(?:[a-z\d]+\.))(?:(?:[^\s()<>]+|\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))?\))+(?:\((?:[^\s()<>]+|(?:\(?:[^\s()<>]+\)))?\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))?'
         try:
             response = urllib.request.urlopen(url)
         except urllib.error.HTTPError as err:
-            print(err.code)
+            print('Error %d: %s' %(err.code, err.reason))
             return
         except urllib.error.URLError as err:
             print('URL Error encountered')
             return
         
         html = response.read()
+        self.size = len(html)
 
-        soup = BeautifulSoup(html, 'html.parser')('a')
+        soup = BeautifulSoup(html, 'html.parser')
 
-        #Converts anchor tags to urls
-        for i, link in enumerate(soup):
-            soup[i] = re.findall('(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})', str(link['href']))
+        #If ASSET_FLAG false, only parse urls within an anchor tag
+        if 0 == self.ASSET_FLAG:
+            linkSoup = soup('a', href=True)
 
-        for link in soup:
-            if link != [] and link[0]:
-                self.links.add(str(link[0]))
+            #Converts anchor tags to urls
+            for i, link in enumerate(linkSoup):
+                linkSoup[i] = re.findall(pattern, str(link))
+
+            for link in linkSoup:
+                if link != [] and link[0]:
+                    #Adds propper formatting for urllib to work
+                    if -1 == str(link[0]).find('http'):
+                        self.links.add(str('http://' + link[0]))
+                    else:
+                        self.links.add(str(link[0]))
+        
+        #If ASSET_FLAG true, parse all links
+        elif 1 == self.ASSET_FLAG:
+            linkSoup = re.findall(pattern, str(soup))
+            for link in linkSoup:
+                self.links.add(link)
+
+        print('\nLink: %s \nDepth: %d \nSize: %d\n'%(url, self.depth, len(html)))
 
         return self.links
 
-    
-
-
-crawler = WebCrawler('https://Runescape.com', 2)
-
-crawler.startCrawl()
+if __name__ == '__main__':
+    crawler = WebCrawler(str(args.url[0]), args.depth[0])
+    crawler.startCrawl()
